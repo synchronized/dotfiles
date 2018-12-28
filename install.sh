3df0bbbfeedace321980a77c1d9747003f5220ae
sunday <synchronized@yeah.net>
2018-12-28 21:13:54 +0800

    安装脚本 nvm

--- install.sh
+++ install.sh
@@ -23,10 +23,26 @@ sd_install_link globalrc .globalrc
 sd_install_link vim .vim
 sd_install_link editorconfig .editorconfig
 
+#-----------------------------------------------------------------------------
+# Install fzf
 if [ ! -d "${HOME}/.fzf" ]
 then
-    echo "Install junegunn/fzf"
+    echo "Install junegunn/fzf ..."
     git clone --depth 1 https://github.com/junegunn/fzf.git ${HOME}/.fzf
     bash ~/.fzf/install --key-bindings --completion --no-update-rc
 fi
 
+#-----------------------------------------------------------------------------
+# Install nvm
+export NVM_DIR="${HOME}/.nvm"
+if [ ! -d "${NVM_DIR}" ]
+then
+    echo "Install nvm ..."
+    (
+        git clone https://github.com/creationix/nvm.git "$NVM_DIR"
+        cd "$NVM_DIR"
+        git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+    ) && \. "$NVM_DIR/nvm.sh"
+    # Install default lts
+    nvm install --lts=Carbon
+fi
