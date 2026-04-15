fdcb8588d312abb393be74bdf455ae7ce725f675
synchronized <synchronized@yeah.net>
2026-04-15 09:27:38 +0800

    去掉不需要的文件

--- example/dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((go-mode
-  ((eval . (progn
-             (setq process-environment (copy-sequence process-environment))
-             (setenv "GOPATH" "/path/of/golang/project"))))
-  (go-backend . go-mode))
- )
